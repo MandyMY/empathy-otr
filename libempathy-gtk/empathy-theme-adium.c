6b2cf166cef57ccbecbb93dcb961675a124926d0
MandyMY <wangmychn@gmail.com>
2017-04-15 15:54:59 +0800

    make error

--- libempathy-gtk/empathy-theme-adium.c
+++ libempathy-gtk/empathy-theme-adium.c
@@ -772,7 +772,7 @@ theme_adium_remove_focus_marks (EmpathyThemeAdium *self,
   for (i = 0; i < webkit_dom_node_list_get_length (nodes); i++)
     {
       WebKitDOMNode *node = webkit_dom_node_list_item (nodes, i);
-      WebKitDOMHTMLElement *element = WEBKIT_DOM_HTML_ELEMENT (node);
+      WebKitDOMElement *element = WEBKIT_DOM_ELEMENT (node);
       gchar *class_name;
       gchar **classes, **iter;
       GString *new_class_name;
@@ -781,7 +781,7 @@ theme_adium_remove_focus_marks (EmpathyThemeAdium *self,
       if (element == NULL)
         continue;
 
-      class_name = webkit_dom_html_element_get_class_name (element);
+      class_name = webkit_dom_element_get_class_name (element);
       classes = g_strsplit (class_name, " ", -1);
       new_class_name = g_string_sized_new (strlen (class_name));
 
@@ -798,7 +798,7 @@ theme_adium_remove_focus_marks (EmpathyThemeAdium *self,
             }
         }
 
-      webkit_dom_html_element_set_class_name (element, new_class_name->str);
+      webkit_dom_element_set_class_name (element, new_class_name->str);
 
       g_free (class_name);
       g_strfreev (classes);
@@ -1112,8 +1112,7 @@ void
 empathy_theme_adium_append_event (EmpathyThemeAdium *self,
     const gchar *str)
 {
-  TpawStringParser *parsers;
-  GString *string;
+  gchar *str_escaped;
   PangoDirection direction;
 
   if (self->priv->pages_loading != 0)
@@ -1123,12 +1122,9 @@ empathy_theme_adium_append_event (EmpathyThemeAdium *self,
     }
 
   direction = pango_find_base_dir (str, -1);
-
-  parsers = empathy_webkit_get_string_parser (FALSE);
-  string = g_string_sized_new (strlen (str));
-  tpaw_string_parser_substr (str, -1, parsers, string);
-  theme_adium_append_event_escaped (self, string->str, direction);
-  g_string_free (string, TRUE);
+  str_escaped = g_markup_escape_text (str, -1);
+  theme_adium_append_event_escaped (self, str_escaped, direction);
+  g_free (str_escaped);
 }
 
 void
